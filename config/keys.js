0627dfea66532f67a5778890f2862c9b5e621e06
Guilherme Araujo <guiluis.aa@gmail.com>
2018-04-17 22:45:44 -0300

    User API Routes & JWT Authentication (until 13. Passport JWT...)

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://guilherme:4715416@ds147589.mlab.com:47589/devconnectordb'
+	mongoURI: 'mongodb://guilherme:4715416@ds147589.mlab.com:47589/devconnectordb',
+	secretOrKey: 'secret'
 }
